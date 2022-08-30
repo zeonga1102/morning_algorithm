68336336dac975886b22181162a753be2b385e9c
zeonga1102 <dlwjddk4587@naver.com>
2022-08-30 14:57:35 +0900

    알고리즘 개선 및 list로 스택 구현

--- day_08/question_01.py
+++ day_08/question_01.py
@@ -10,11 +10,13 @@ class Node:
 
 class Stack:
     head = None
+    count = 0
 
     def push(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+        self.count += 1
 
     def pop(self):
         if self.head is None:
@@ -22,15 +24,10 @@ class Stack:
         else:
             print(self.head.data)
             self.head = self.head.next
+            self.count -= 1
 
-    def size(self):
-        cur_node = self.head
-        count = 0
-        while cur_node is not None:
-            count += 1
-            cur_node = cur_node.next
-        
-        print(count)
+    def size(self):        
+        print(self.count)
 
     def empty(self):
         if self.head is None:
@@ -64,3 +61,31 @@ for i in range(n):
         stack.empty()
     elif cmd == 'top':
         stack.top()
+
+
+# n = int(sys.stdin.readline())
+
+# stack = []
+
+# for i in range(n):
+#     cmd = sys.stdin.readline().split()
+
+#     if cmd[0] == 'push':
+#         stack.append(cmd[1])
+#     elif cmd[0] == 'pop':
+#         if len(stack) < 1:
+#             print(-1)
+#         else:
+#             print(stack.pop())
+#     elif cmd[0] == 'size':
+#         print(len(stack))
+#     elif cmd[0] == 'empty':
+#         if len(stack) < 1:
+#             print(1)
+#         else:
+#             print(0)
+#     elif cmd[0] == 'top':
+#         if len(stack) < 1:
+#             print(-1)
+#         else:
+#             print(stack[-1])
