e7c4713b7b95739bdafc7eefd622b25893595b43
zeonga1102 <dlwjddk4587@naver.com>
2022-09-01 03:52:26 +0900

    3번 해결

--- day_10/question_03.py
+++ day_10/question_03.py
@@ -1,1 +1,25 @@
 # https://www.acmicpc.net/problem/1021
+
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+
+location = list(map(int, sys.stdin.readline().split()))
+
+dq = deque([i for i in range(1, n+1)])
+
+count = 0
+for i in location:
+    while dq[0] != i:
+        if dq.index(i) < len(dq) / 2:
+            while dq[0] != i:
+                dq.rotate(-1)
+                count += 1
+        else:
+            while dq[0] != i:
+                dq.rotate(1)
+                count += 1
+    dq.popleft()
+    
+print(count)
