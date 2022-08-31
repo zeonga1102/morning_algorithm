9524289ac46b3dc7dfcafa1a41520863990c9a95
zeonga1102 <dlwjddk4587@naver.com>
2022-09-01 03:52:20 +0900

    2번 해결

--- day_10/question_02.py
+++ day_10/question_02.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/1874
+
+import sys
+
+n = int(sys.stdin.readline())
+
+sequence = []
+for i in range(n):
+    sequence.append(int(sys.stdin.readline()))
+
+stack = []
+cur_num = 1
+result = ''
+for i in range(n):
+    while cur_num <= sequence[i]:
+        stack.append(cur_num)
+        cur_num += 1
+        result += '+'
+    
+    if stack[-1] == sequence[i]:
+        stack.pop()
+        result += '-'
+    else:
+        print('NO')
+        break
+else:
+    for i in result:
+        print(i)
