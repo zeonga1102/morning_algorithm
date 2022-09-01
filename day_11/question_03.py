bf743ac8b15816ab5b4754f06a3b35105f78ff0a
zeonga1102 <dlwjddk4587@naver.com>
2022-09-01 21:24:50 +0900

    3번 해결

--- day_11/question_03.py
+++ day_11/question_03.py
@@ -1,1 +1,24 @@
 # https://www.acmicpc.net/problem/2108
+
+import sys
+from collections import Counter
+
+
+n = int(sys.stdin.readline())
+
+num_list = []
+for _ in range(n):
+    num_list.append(int(sys.stdin.readline()))
+
+num_list.sort()
+
+print(int(round(sum(num_list)/n)))
+print(num_list[len(num_list)//2])
+
+count = Counter(num_list).most_common(2)
+if count[0][1] == count[1][1]:
+    print(count[1][0])
+else:
+    print(count[0][0])
+
+print(num_list[-1] - num_list[0])
